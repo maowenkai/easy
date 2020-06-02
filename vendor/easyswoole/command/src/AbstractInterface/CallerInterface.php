66a11620cbe9d45e6e802c3d122ab2d516562787
power0904 <power0904@163.com>
2020-06-02 19:56:19 +0800

    fx

---
+++ vendor/easyswoole/command/src/AbstractInterface/CallerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace EasySwoole\Command\AbstractInterface;
+
+
+interface CallerInterface
+{
+    public function getCommand():string;
+    public function setCommand(string $command);
+    public function setParams($params);
+    public function getParams();
+}
